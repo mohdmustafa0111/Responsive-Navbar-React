b9cd9cfef7460e6adc218c1e3a0b3bfbb083e996
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2025-02-19 20:14:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+const App = () => {
+  return (
+    <div className="flex justify-between items-center p-5 bg-gray-100 shadow-xl">
+      {/* Logo */}
+      <div className="ml-10">LOGO</div>
+
+      {/* Desktop Icon */}
+      <div className="space-x-10 mr-10">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Services</a>
+        <a href="">Contact Us</a>
+      </div>
+
+      {/* Hamburger Icon */}
+
+      {/* Mobile Icon */}
+    </div>
+  );
+};
+
+export default App;
